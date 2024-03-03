3d34d709066cbde121d8ea3c58b8ecd9a0c1d8d9
Florian Schroedl <flo.schroedl@gmail.com>
2024-03-03 16:21:32 +0100

    Use component directly for now

--- src/components/Home/VueHome.vue
+++ src/components/Home/VueHome.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import CommandPromptRoot from '@/components/CommandPrompt/CommandPromptRoot.vue';
+// import CommandPromptRoot from '@/components/CommandPrompt/CommandPromptRoot.vue';
+import CommandPromptView from '@/components/CommandPrompt/CommandPromptView.vue';
 </script>
 
 <template>
-    <CommandPromptRoot />
+    <CommandPromptView />
 </template>
