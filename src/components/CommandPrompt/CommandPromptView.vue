758e8715de37992c4b1573dde007dd2e8a88cd75
Florian Schroedl <flo.schroedl@gmail.com>
2024-03-04 20:22:16 +0100

    Fix item not logging in deeper view stack

--- src/components/CommandPrompt/CommandPromptView.vue
+++ src/components/CommandPrompt/CommandPromptView.vue
@@ -97,8 +97,9 @@ const selectPrev = (): void => {
 };
 
 const submit = (id?: string): void => {
+    const flattenedItems = Object.values(currentItems.value!).flat();
     const targetId = id || selected.value;
-    const item = mainItems.find((x) => x.id === targetId);
+    const item = flattenedItems.find((x) => x.id === targetId);
 
     if (!item) {
         return;
